3f099cd77fc7c83fde3c1ea434f95a33ab57226f
lc-d <lorenzo@cataldi.design>
2023-02-08 10:55:09 +0100

    add favicon

--- app.vue
+++ app.vue
@@ -10,9 +10,46 @@
 </template>
 
 <script setup>
-  useHead({
-    titleTemplate: '%s | Latitude X',
-  })
+useHead({
+  titleTemplate: '%s | Latitude X',
+  htmlAttrs: {
+    lang: 'it',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'format-detection', content: 'telephone=no' },
+  ],
+  link: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/logo/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/logo/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/logo/favicon-16x16.png',
+    },
+     {
+        rel: 'mask-icon',
+        color: '#5bbad5',
+        href: '/logo/safari-pinned-tab.svg'
+      },
+    {
+      rel: 'manifest',
+      crossorigin: 'use-credentials',
+      href: '/logo/site.webmanifest',
+    },
+  ],
+})
 </script>
 
 <style>
