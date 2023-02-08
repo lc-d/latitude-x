0151c9a809cc1700d376bde91f44f20fe97a2dd5
lc-d <lorenzo@cataldi.design>
2023-02-08 14:08:37 +0100

    remove day from date

--- plugins/helpers.js
+++ plugins/helpers.js
@@ -4,7 +4,6 @@ export default defineNuxtPlugin(nuxtApp => {
           date: (date) => {
             const options = {
               month: 'short',
-              day: '2-digit',
               year: 'numeric'
             }
             return new Date(date).toLocaleString(['it-IT'], options)
